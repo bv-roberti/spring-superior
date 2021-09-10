895406fe7cdcd656d1df3b772faf386b312317b2
Bruno <broberti@ripple.com>
2021-09-10 14:24:13 -0300

    charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,36 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import BarChart from "components/BarChart";
+import React from "react";
+import DonutChart from "components/DonutChart";
+
 function App() {
   return (
-    <div >
+    < > 
+    <NavBar />
+     <div className="container">
       <h1 className="text-primary">Hello world!</h1>
-    </div>
+      </div>
+
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5>All sales</h5>
+          <BarChart />
+        </div>
+        <div className="col-sm-6">
+          <h5>All sales</h5>
+          <DonutChart />
+        </div>
+      </div>
+
+      <div className="py-3">
+        <h2 className="text-primary">All sales</h2>
+      </div>
+
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
